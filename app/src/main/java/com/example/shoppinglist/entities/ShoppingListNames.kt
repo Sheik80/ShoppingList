7ef86d015529dde5f710d9670d1aa60530fea87f
Sheykin Alexey <alexey.sheykin@nordgold.com>
2023-01-04 11:45:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/entities/ShoppingListNames.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_lis_names")
+data class ShopListNames(
+    @Primary
+)
